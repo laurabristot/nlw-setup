c77607ce0233985e3526b9f092ee593ee997c26b
laura bristot <laurabristotbarbi@hotmail.com>
2023-01-18 17:40:01 -0300

    Aula 2 parte Mobile

---
+++ mobile/src/utils/generate-dates-from-year-beginning.tsx
@@ -0,0 +1,17 @@
+import dayjs from 'dayjs'
+
+export function generateDatesFromYearBeginning() {
+  const firstDayOfTheYear = dayjs().startOf('year')
+  const today = new Date()
+
+  const dates = []
+
+  let compareDates = firstDayOfTheYear
+
+  while (compareDates.isBefore(today)) {
+    dates.push(compareDates.toDate())
+    compareDates = compareDates.add(1, 'day')
+  }
+
+  return dates
+}
